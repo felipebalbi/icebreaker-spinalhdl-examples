3990b2064a78b03bc62902f10bc55e04766f27c9
Felipe Balbi <felipe@balbi.sh>
2026-05-02 16:28:36 -0700

    Add blinky with reset signal
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ BlinkyWithReset.scala
@@ -0,0 +1,32 @@
+import spinal.core._
+
+case class BlinkyWithReset() extends Component {
+  val io = new Bundle {
+    val clk = in Bool()
+    val reset = in Bool()
+    val led = out Bool()
+  }
+
+  val cd = ClockDomain(
+    clock = io.clk,
+    reset = io.reset,
+    config = ClockDomainConfig(
+      resetKind = SYNC,
+      resetActiveLevel = LOW,
+    )
+  )
+
+  val area = new ClockingArea(cd) {
+    val counter = Reg(UInt(24 bits)) init(0)
+    counter := counter + 1
+    io.led := counter.msb
+  }
+}
+
+object BlinkyWithResetVerilog {
+  def main(args: Array[String]): Unit = {
+    SpinalConfig(
+      targetDirectory = "rtl"
+    ).generateVerilog(BlinkyWithReset())
+  }
+}
