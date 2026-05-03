d6a7b0038bf5e1827e7be6b5266c7eb8038558c1
Felipe Balbi <felipe@balbi.sh>
2026-05-03 16:13:38 -0700

    Add ButtonDebouncer
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ ButtonDebouncer/src/DebouncerConfig.scala
@@ -0,0 +1,14 @@
+package button
+
+import spinal.core._
+
+object DebounceKind extends SpinalEnum {
+  val Integrator, Timer = newElement()
+}
+
+case class DebouncerConfig(
+  kind: DebounceKind.E,
+  width: Int = 16,
+  clkFreqHz: Int = 12_000_000,
+  debounceMs: Int = 10
+)
